4a4fac5c51918fa4eae4a18a6b327ce5661e3d19
Nihad-roshan <nihadroshanm@gmail.com>
2025-09-02 10:13:55 +0530

    Update 1143.c

--- 1143.c
+++ 1143.c
@@ -1,17 +1,21 @@
 
-int longestCommonSubsequence(char* text1, char* text2) {
+int longestCommonSubsequence(char* text1, char* text2) 
+{
     int n = strlen(text1);
     int m = strlen(text2);
 
     // Allocate DP table
     int **dp = (int**)malloc((n+1) * sizeof(int*));
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++) 
+    {
         dp[i] = (int*)calloc(m+1, sizeof(int));
     }
 
     // Fill DP table
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= m; j++) 
+        {
             if (text1[i-1] == text2[j-1])
                 dp[i][j] = 1 + dp[i-1][j-1];
             else
@@ -22,8 +26,10 @@ int longestCommonSubsequence(char* text1, char* text2) {
     int result = dp[n][m];
 
     // Free memory
-    for (int i = 0; i <= n; i++) free(dp[i]);
+    for (int i = 0; i <= n; i++) 
+        free(dp[i]);
     free(dp);
 
     return result;
 }
+
