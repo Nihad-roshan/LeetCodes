a9c185b6276143f02b4d8204d6c83562058776a6
Nihad-roshan <nihadroshanm@gmail.com>
2025-09-18 10:11:22 +0530

    239.c

--- 239.c
+++ 239.c
@@ -11,13 +11,18 @@ int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize)
     int* result=(int*)malloc((*returnSize)*sizeof(int));
     int* deque=(int*)malloc(numsSize*sizeof(int));
     int front=0,back=0;
-    for(int i=0;i<numsSize;i++){
-        if(front<back&&deque[front]<=i-k)front++;
-        while(front<back&&nums[deque[back-1]]<=nums[i])back--;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(front<back&&deque[front]<=i-k)
+            front++;
+        while(front<back&&nums[deque[back-1]]<=nums[i])
+            back--;
         deque[back++]=i;
-        if(i>=k-1)result[i-k+1]=nums[deque[front]];
+        if(i>=k-1)
+            result[i-k+1]=nums[deque[front]];
     }
     free(deque);
     return result;
+
 
 }
